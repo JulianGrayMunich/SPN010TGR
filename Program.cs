c1e76f2bf07dcbcd29a9a33da16bd234f05b0ecd
JulianGrayMunich <gna.geomatics@gmail.com>
2022-10-25 11:00:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using databaseAPI;
+using EASendMail;
+using GNAgeneraltools;
+using GNAspreadsheettools;
+using GNAsurveytools;
+
+
+
+
+
+using OfficeOpenXml;
+using System.Configuration;
+using System.Globalization;
+using Microsoft.Win32;
+using static OfficeOpenXml.ExcelErrorValue;
+
+namespace BBVTGR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+        }
+    }
+}
